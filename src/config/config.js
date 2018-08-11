9f8a2966b4a91c0cd9339636a352341fbe1de6ca
Masha Reko <mmreko@gmail.com>
2018-08-11 20:36:13 +0200

    Test dns

--- src/config/config.js
+++ src/config/config.js
@@ -5,8 +5,9 @@ const serverSettings = {
 
 // user management service parameters
 const userManagementOptions = {
-	host: "10.108.219.82",
-	port: 80
+	//host: "10.108.219.82",
+	host: "http://user-management"
+	port: 3001
 }
 
 module.exports = Object.assign({}, { serverSettings, userManagementOptions })
